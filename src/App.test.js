226a601b06d15dffe373f16c7ef280ba586eb631
Josh Beasley <josh.beasley@hotmail.com>
2022-11-02 13:20:06 -0700

    completed

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,49 @@
+//src App.test.js
+import React from 'react';
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import App from './App';
 
-test('renders learn react link', () => {
+test('Add recipe button toggles visibility of a form on the page', async () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  // `queryBy...` methods will return null if the element is not found:
+  const recipeForm = screen.queryByText("Instructions:");
+
+  // `getBy...` methods will "throw" an error if the element is not found:
+  // const recipeForm = screen.getByText("Instructions:");
+
+  expect(recipeForm).toBeNull();
+  await userEvent.click(screen.getByText("Add Recipe"));
+
+  expect(screen.getByLabelText("Instructions:")).toBeInTheDocument();
 });
+
+// src/App.test.js
+test('typing in the recipe name makes the recipe name appear in the input', async () => {
+  render(<App />);
+
+  const recipeName = 'No pockets';
+  await userEvent.click(screen.getByText("Add Recipe"));
+  await userEvent.type(screen.getByLabelText('Recipe name:'), recipeName);
+
+  expect(screen.getByLabelText('Recipe name:').value).toEqual(recipeName);
+})
+
+const setup = async () => {
+  render(<App />);
+
+  await userEvent.click(screen.getByText('Add Recipe'));
+  const instructionsInput = screen.getByLabelText('Instructions:')
+  return {
+    instructionsInput,
+  }
+}
+
+test('typing in the recipe instructions makes the instructions appear in the form', async () => {
+  const {instructionsInput} = await setup();
+
+  const recipeInstructions = "kinda hard to write instructions without knowing what I'm cooking"
+
+  await userEvent.type(instructionsInput, recipeInstructions)
+  expect(instructionsInput.value).toEqual(recipeInstructions);
+})
